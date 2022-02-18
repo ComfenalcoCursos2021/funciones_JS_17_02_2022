506d6321da6d60fd77b2b5dfeec36a73231f5ddb
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-02-17 21:12:15 -0500

    Funciones saludar a 5 personas

--- main.js
+++ main.js
@@ -1,26 +1,10 @@
-
-let num = 1, nombre = null;
-
-nombre = prompt(`Hola soy el robot del JS como te llamas usuario ${num}`);
-alert(`Hola usuario ${nombre} me alegra conocerte`);
-
-num += 1;
-
-nombre = prompt(`Hola soy el robot del JS como te llamas usuario ${num}`);
-alert(`Hola usuario ${nombre} me alegra conocerte`);
-
-num += 1;
-
-nombre = prompt(`Hola soy el robot del JS como te llamas usuario ${num}`);
-alert(`Hola usuario ${nombre} me alegra conocerte`);
-
-num += 1;
-
-nombre = prompt(`Hola soy el robot del JS como te llamas usuario ${num}`);
-alert(`Hola usuario ${nombre} me alegra conocerte`);
-
-num += 1;
-
-nombre = prompt(`Hola soy el robot del JS como te llamas usuario ${num}`);
-alert(`Hola usuario ${nombre} me alegra conocerte`);
-
+saludarRobot(1);
+saludarRobot(2);
+//Funcion por EXPRESION
+function saludarRobot(num) {
+    let nombre = prompt(`Hola soy el robot del JS como te llamas usuario ${num}`);
+    alert(`Hola usuario ${nombre} me alegra conocerte`);
+}
+saludarRobot(3);
+saludarRobot(4);
+saludarRobot(5);
