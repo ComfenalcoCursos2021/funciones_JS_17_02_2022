4259fba70e56d514fb04aed687e3171d3421458f
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-02-17 20:50:23 -0500

    Saludar a 5 personas en secuencia

---
+++ main.js
@@ -0,0 +1,26 @@
+
+let num = 1, nombre = null;
+
+nombre = prompt(`Hola soy el robot del JS como te llamas usuario ${num}`);
+alert(`Hola usuario ${nombre} me alegra conocerte`);
+
+num += 1;
+
+nombre = prompt(`Hola soy el robot del JS como te llamas usuario ${num}`);
+alert(`Hola usuario ${nombre} me alegra conocerte`);
+
+num += 1;
+
+nombre = prompt(`Hola soy el robot del JS como te llamas usuario ${num}`);
+alert(`Hola usuario ${nombre} me alegra conocerte`);
+
+num += 1;
+
+nombre = prompt(`Hola soy el robot del JS como te llamas usuario ${num}`);
+alert(`Hola usuario ${nombre} me alegra conocerte`);
+
+num += 1;
+
+nombre = prompt(`Hola soy el robot del JS como te llamas usuario ${num}`);
+alert(`Hola usuario ${nombre} me alegra conocerte`);
+
