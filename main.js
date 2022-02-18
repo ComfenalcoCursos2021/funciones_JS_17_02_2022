2c80d40eb28b681c5629c91c99d66c7f96efea7d
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-02-17 21:37:49 -0500

    Funciones saludar a 5 personas DECARACIO

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-saludarRobot(1);
-saludarRobot(2);
-//Funcion por EXPRESION
-function saludarRobot(num) {
+//Funcion por DECLARACION
+const saludar = function saludarRobot(num) {
     let nombre = prompt(`Hola soy el robot del JS como te llamas usuario ${num}`);
     alert(`Hola usuario ${nombre} me alegra conocerte`);
-}
-saludarRobot(3);
-saludarRobot(4);
-saludarRobot(5);
+};
+saludar(1);
+saludar(2);
+saludar(3);
+saludar(4);
+saludar(5);
